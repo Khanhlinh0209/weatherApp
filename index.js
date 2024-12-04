c6b6175e8a9c61f94ff366e88a76811e4fcf554f
khanhlinhwork1110@gmail.com <khanhlinhwork1110@gmail.com>
2024-12-05 01:02:16 +0700

    fix

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const form = document.querySelector(".container form");
 const inputCity = document.querySelector(".inputCity");
+const citiesContainer = document.querySelector(".cities");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const cityName = inputCity.value.trim(); // Lấy tên thành phố từ input
   if (cityName) {
     console.log("City Name:", cityName);
+
+    // //Kiểm tra thành phố có trùng không
+    // const listItems = citiesContainer.querySelectorAll(".city");
+    // const listItemsArray = Array.from(listItems);
+
+    // const filteredArray = listItemsArray.filter((item) => {
+    //   let content = "";
+    //   const cityNameSpan = item.querySelector(".city-name span");
+    //   if (cityNameSpan) {
+    //     content = cityNameSpan.textContent.toLowerCase().trim(); // Chuẩn hóa tên thành phố từ UI
+    //   } else {
+    //     content = ""; // Nếu không tìm thấy phần tử, gán content là chuỗi rỗng
+    //   }
+
+    //   return content === cityName;
+    // });
+    // console.log("Filtered Array:", filteredArray);
+    // console.log("List cities: ", listItemsArray);
+
+    // //Xử lý nếu thành phố đã tồn tại
+
+    // if (filteredArray.length > 0) {
+    //   alert(
+    //     `You already know the weather for ${
+    //       filteredArray[0].querySelector(".city-name span").textContent
+    //     }`
+    //   );
+    //   form.reset();
+    //   inputCity.focus();
+    //   return;
+    // }
 
     const apiKey = "115ad39c5e6db1215b30f60bc2b94b9a";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&&units=metric`;
@@ -53,7 +85,7 @@ const additemToUI = (data) => {
   const markup = `
         <div class="weather-card">
             <div class="location">
-              <span>${name}</span>
+              <span class="city-name">${name}</span>
             </div>
             <div class="temperature">
               <span>${Math.round(main.temp)}</span>
